23a0dfe8418df887658980ab4adc81e92f3836e1
aminsultan697 <aminsultan697@gmail.com>
2026-04-20 23:04:19 +0500

    Update LAB 2 work 3.cpp

--- LAB 2/LAB WORK/LAB 2 work 3.cpp
+++ LAB 2/LAB WORK/LAB 2 work 3.cpp
@@ -1,7 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
+// Name: Amin Sultan 
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0737
+// Section: A
 
 
 
